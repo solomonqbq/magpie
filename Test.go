24ad1f035f9b3558d6fe8af141e544ae6ea6bb78
秦宝齐 <qinbaoqi@jd.com>
2015-04-01 18:50:13 +0800

    删除无用代码

--- Test.go
+++ Test.go
@@ -10,9 +10,5 @@ func main() {
 	fmt.Println(global.GetFirstLocalIP())
 	s := strings.SplitN("a=th=45", "=", 2)
 	fmt.Println(s)
-	//	macs, _ := global.GetLocalMac()
-	//	for _, mac := range macs {
-	//		fmt.Println(mac)
-	//	}
 
 }
